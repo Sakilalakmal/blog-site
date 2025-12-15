20108adbd1194777c7415274350b58f774fad64f
Ganwari Nakathige Sakila Lakmal Nandasiri CB014383 <164737611+Sakilalakmal@users.noreply.github.com>
2025-12-15 16:33:21 +0530

    feat: add Create Blog functionality with form validation and API integration

---
+++ convex/posts.ts
@@ -0,0 +1,22 @@
+import { mutation } from "./_generated/server";
+import { ConvexError, v } from "convex/values";
+import { authComponent } from "./auth";
+
+// Create a new task with the given text
+export const createBlogPost = mutation({
+  args: { title: v.string(), content: v.string() },
+  handler: async (ctx, args) => {
+    const user = await authComponent.safeGetAuthUser(ctx);
+
+    if (!user) {
+      throw new ConvexError("Unauthorized");
+    }
+    const blogArticle = await ctx.db.insert("posts", {
+      title: args.title,
+      content: args.content,
+      authorId: user._id,
+    });
+
+    return blogArticle;
+  },
+});
